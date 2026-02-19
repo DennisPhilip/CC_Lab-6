50e1f7087a8d14e30c25008b5c91299e43ea2481
root <root@DP-ZenBook.>
2026-02-19 17:40:18 +0000

    Fixed backend structure

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socket
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        hostname = socket.gethostname()
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(f"Served by backend: {hostname}".encode())
+
+server = HTTPServer(("0.0.0.0", 8080), Handler)
+print("Backend running on port 8080")
+server.serve_forever()
